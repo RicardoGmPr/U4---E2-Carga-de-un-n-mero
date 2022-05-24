6b0816e8cb42e6a9f28bc5acc8336072e895978b
RicardoGmPr <70128085+RicardoGmPr@users.noreply.github.com>
2022-05-24 14:15:49 -0700

    Add files via upload

---
+++ U4 - E2 Carga de un número/U4 - E2 Carga de un número/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4___E2_Carga_de_un_número
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            try
+            {
+                Console.Write("Indique un numero: "); 
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Pow(num1, 2)); 
+                Console.ReadKey();
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Tiene que ingresar un número entero");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("El número ingresado es demasiado grande");
+            }
+            finally
+            {
+
+            }       
+        }
+    }
+}
